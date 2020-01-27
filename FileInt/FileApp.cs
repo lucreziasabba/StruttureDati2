97e9d9ae254d3ea3bbf999b850f1498d017596aa
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:45:12 +0100

    Add project files.

---
+++ FileInt/FileApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileInt
+{
+    class FileApp
+    {
+        private const string file = "file.txt";
+        private const string file2 = "finito.txt";
+        static void Main(string[] args)
+        {
+            HashSet<int> numeri = new HashSet<int>();
+            using (StreamReader x = new StreamReader(file, Encoding.UTF8))
+            {
+                string line;
+                while ((line = x.ReadLine()) != null)
+                {
+                    int n = int.Parse(line);
+                    numeri.Add(n);
+                }
+            }
+            using (StreamWriter f = new StreamWriter(file2, false))
+            {
+                foreach (int d in numeri)
+                    f.WriteLine(d);
+            }
+        }
+    }
+}
